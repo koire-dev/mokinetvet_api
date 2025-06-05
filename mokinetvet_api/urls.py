8ff15fd5463f342a647b801a5b944de2374a8ee6
Célestin BILONG NTOUBA <celestin.ntouba@ngc-consult.com>
2025-06-05 12:01:15 +0100

    Livraisons des apis mokinet v02

--- mokinetvet_api/urls.py
+++ mokinetvet_api/urls.py
@@ -23,6 +23,7 @@ schema_view = get_schema_view(
 print(">>> URLs chargés pour la prod <<<")
 
 urlpatterns = [
+    # path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('animals.urls')),
     path('api/', include('consultations.urls')),
@@ -39,5 +40,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
